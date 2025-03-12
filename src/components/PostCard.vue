94573e2acf9bfa31548fa8d5b6a7427bb2e274dd
Noé <noebrault@live.fr>
2025-03-12 10:54:07 +0100

    Api and routes

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,10 +1,14 @@
 <script setup>
     import {HeartIcon, TrashIcon} from "@heroicons/vue/24/outline";
+    import {HeartIcon as SolidHeartIcon} from "@heroicons/vue/24/solid";
+    import {useRouter} from "vue-router";
+
+    const router = useRouter();
 
     const props = defineProps({
         post: {
             type: Object,
-            reauired: true,
+            required: true,
         }
     })
 
@@ -18,19 +22,29 @@
         emit("delete", props.post.id)
     }
 
+    function goToUserProfile(){
+        router.push({name: "user", params: {username: props.post.author.username}})
+    }
+
 </script>
 
 <template>
     <article class="card">
         <header>
-          <img :src="post.author.avatarUrl" alt="avatar" width="36" height="36">
-          <a>{{ post.author.username }}</a>
+            <img :src="post.author.avatarUrl" alt="avatar" width="36" height="36">
+            <a @click="goToUserProfile">{{ post.author.username }}</a>
+            <!--
+                <a>{{ post.createdAt }}</a>
+            -->
         </header>
         <p>{{ post.content }}</p>
-        <p>Le post il est aimé ? {{ post.liked }}</p>
         <footer>
             <button class="btn-icon" @click="likePost">
-                <HeartIcon />
+                <!--
+                    <HeartIcon v-if="!post.liked"/>
+                    <SolidHeartIcon v-else class="active"/>
+                -->
+                <component :is="post.liked ? SolidHeartIcon : HeartIcon" :class="{active: post.liked}"/>
             </button>
             <button class="btn-icon" @click="deletePost">
                 <TrashIcon />
